b74280007df051c56e59f58d3a09853eaf643da9
Justin Yu <justin.v.yu@gmail.com>
2015-08-01 12:41:59 -0700

    JSify

--- client/views/faq/faq.js
+++ client/views/faq/faq.js
@@ -1,11 +1,41 @@
+
+var frequentlyAskedQuestions = [
+	{
+		question: "Question 1",
+		answer: "Answer 1"
+	},
+	{
+		question: "Question 2",
+		answer: "Answer 2"
+	},
+	{
+		question: "Question 3",
+		answer: "Answer 3"
+	},
+	{
+		question: "Question 4",
+		answer: "Answer 4"
+	},
+	{
+		question: "Question 5",
+		answer: "Answer 5"
+	},
+	{
+		question: "Question 6",
+		answer: "Answer 6"
+	}
+]
+
+
+
 Controller('faq', {
+		helpers: {
 
-    helpers: {
+			faqs: frequentlyAskedQuestions
+			
+		},
 
+		rendered: function() {
 
-    },
-
-    rendered: function() {
-    }
-
-});
+		}
+})
