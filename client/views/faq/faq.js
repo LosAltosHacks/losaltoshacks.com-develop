c642e57ba8c1b52618286a90cce6ca68f897e238
Dan Appel <Dan.appel00@gmail.com>
2015-08-13 10:36:19 -0700

    fix faq questions to be syntatically valid

--- client/views/faq/faq.js
+++ client/views/faq/faq.js
@@ -2,13 +2,11 @@
 var frequentlyAskedQuestions = [
 	{
 		question: "What is a hackathon?",
-		answer: "A hackathon is an event where people can get together and code an entirely new project. 
-		 Los Altos Hacks will give you roughly 24 hours to make your idea become a reality."
+		answer: "A hackathon is an event where people can get together and code an entirely new project. Los Altos Hacks will give you roughly 24 hours to make your idea become a reality."
 	},
 	{
 		question: "Who can participate?",
-		answer: "All high school students can participate.  
-		However, university students and those already graduated can come help mentor."
+		answer: "All high school students can participate. However, university students and those already graduated can come help mentor."
 	},
 	{
 		question: "I'm not from Los Altos High School, can I still participate?",
@@ -16,25 +14,17 @@ var frequentlyAskedQuestions = [
 	},
 	{
 		question: "What should I bring? Will I need my own laptop?",
-		answer: "All attendees are required to bring a valid student ID, which will be checked at registration. 
-		Bring your laptop, phone, hardware, chargers, or anything else you need for the weekend, 
-		since the Los Altos Hacks team will not be able to provide these for you.  
-		Power strips, ethernet cords, sleeping bags, and toiletries are highly recommended."
+		answer: "All attendees are required to bring a valid student ID, which will be checked at registration. Bring your laptop, phone, hardware, chargers, or anything else you need for the weekend, since the Los Altos Hacks team will not be able to provide these for you. Power strips, ethernet cords, sleeping bags, and toiletries are highly recommended."
 	},
 	{
 		question: "When and how will I know that I’ve been accepted?",
-		answer: "Applications will go live during early October, and decisions will be sent out by late October. 
-		Please keep in mind that we are not only accepting those with coding abilities,
-		so if you can’t code, that’s totally fine.  
-		We’re looking to put on a hackathon for people with differing skill levels."
+		answer: "Applications will go live during early October, and decisions will be sent out by late October.\nPlease keep in mind that we are not only accepting those with coding abilities, so if you can’t code, that’s totally fine. We’re looking to put on a hackathon for people with differing skill levels."
 	},
 	{
 		question: "How do I form or join a team?",
-		answer: "You can either apply with your friends and specify on the application, or you can form teams the day of.  
-		If you don’t have a team, don’t worry! We’ll have a session where you can meet others and form teams.  
-		The maximum amount of people on a team is 4 people."
+		answer: "You can either apply with your friends and specify on the application, or you can form teams the day of. If you don’t have a team, don’t worry! We’ll have a session where you can meet others and form teams. The maximum amount of people on a team is 4 people."
 	}
-]
+];
 
 
 
@@ -42,10 +32,10 @@ Controller('faq', {
 		helpers: {
 
 			faqs: frequentlyAskedQuestions
-			
+
 		},
 
 		rendered: function() {
 
 		}
-})
+});
