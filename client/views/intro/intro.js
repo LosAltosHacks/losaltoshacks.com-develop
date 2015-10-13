61f931610115eaf023449433225d4a09bd259a0d
Justin Yu <justin.v.yu@gmail.com>
2015-10-12 17:17:52 -0700

    Change navbar styles

--- client/views/intro/intro.js
+++ client/views/intro/intro.js
@@ -4,27 +4,38 @@ Controller('intro', {
 
     	title: "Los Altos Hacks",
     	location: "Hillview Community Center",
-	date: "January 30th-31th, 2016"
+	    date: "January 30th-31th, 2016"
 
     },
 
     rendered: function() {
         // setupAffix();
-        
+
         $(".nav-menu").click(function() {
             $("#nav-sections").show();
             setTimeout(function() {
               $("#nav-sections").addClass("active");
             }, 50);
-        }); 
+        });
 
         var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
 
-        if(is_chrome === false) {
-            $.stellar();
-        } else {
-            $("#intro").css("background-attachment", "fixed");
-        }
+        // if(is_chrome === false) {
+        //     $.stellar();
+        // } else {
+        //     $("#intro").css("background-attachment", "fixed");
+        // }
+
+        var navbar = document.getElementById("nav");
+        window.addEventListener("scroll", function() {
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            if(scrollTop > 100) {
+                navbar.style["background-color"] = "#222";
+            }
+            else {
+                navbar.style["background-color"] = "transparent";
+            }
+        });
     }
 
 });
