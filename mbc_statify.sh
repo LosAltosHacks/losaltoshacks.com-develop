743505d260aaa664b4ab55e130fc9b727d8bf516
Ryan Huang <npn.git@gmail.com>
2015-10-29 20:23:23 -0700

    Make mbc_statify cross platform, improve usability
    Added better usage help and input validation.

--- mbc_statify.sh
+++ mbc_statify.sh
@@ -1,46 +1,31 @@
 #!/bin/bash
 
-set -e
+# Exit if command returns non-zero value or unset variable is used
+set -eu
 
 if [ $# -ne 1 ]; then
-    echo "usage: $0 [static repo]"
+    echo "usage: $0 [path to static repo]"
     exit 1
 fi
 
-platform=$(uname)
-devDir=$(pwd)
-
-if [ $platform == "Darwin" ]; then
-  staticDir=$(greadlink -f $1) # greadlink for mac (requires brew install coreutils)
-else
-  staticDir=$(readlink -f $1)
+if [ ! -d "$1" ]; then
+    echo "Static repo directory \"$1\" does not exist."
+    exit 1
 fi
 
-#Does meteor-build-client exist?
-command -v meteor-build-client > /dev/null 2>&1
+staticDir=$1
 
-if [ $? -ne 0 ]; then
+#Does meteor-build-client exist?
+if ! command -v meteor-build-client > /dev/null 2>&1; then
     echo "Could not find meteor-build-client. Make sure it is installed."
     exit 1
 fi
 
-# Must be relative becomes meteor-build-client does not support absolute paths...
-#mbcDir=$(mktemp -d --tmpdir=..) doesn't work on mac
+# Must be relative becomes meteor-build-client does not support absolute paths
+# Provide explicit template for compatibility with BSD mktemp
+mbcDir=$(mktemp -d tmp.XXXXXXXXXX)
 
-# To support both darwin and linux: http://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
-mbcDir=$(mktemp -d 2>/dev/null || mktemp -d -t '/tmp/') # doesn't work
-
-echo $mbcDir
-
-cd $devDir
 meteor-build-client $mbcDir
-
-# Change to absolute path
-if [ $platform == "Darwin" ]; then
-  mbcDir=$(greadlink -f $mbcDir) # greadlink for mac (requires brew install coreutils)
-else
-  mbcDir=$(readlink -f $mbcDir)
-fi
 
 cd $mbcDir
 
@@ -51,8 +36,11 @@ sed index.html -i -e 's/href="\/.\+\.css?.\+true"/href="style\.css"/'
 mv -v $(echo "*.js") "script.js"
 sed index.html -i -e 's/src="\/.\+\.js"/src="script\.js"/'
 
-cp -rf * $staticDir
 cd ..
+
+# Copy the contents of mbcDir to staticDir recursively, only copying changed files
+# Trailing slash needed for rsync to copy contents of directory, not directory itself
+rsync -tr $mbcDir/ $staticDir
 rm -rf $mbcDir
 
 echo -e "\nFiles should now be updated in $staticDir. Please inspect them and commit when ready."
