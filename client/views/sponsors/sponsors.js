7a54a9d0781bde1c050e9ebd86978fb4bf9dcb5c
Ryan Huang <npn.git@gmail.com>
2015-11-12 18:41:55 -0800

    Add Samsung logo and refactor sponsors

---
+++ client/views/sponsors/sponsors.js
@@ -0,0 +1,33 @@
+Controller('sponsors', {
+    helpers: {
+        sponsor: [
+            {
+                name: "Adatao",
+                image: "img/adatao.png",
+                url: "http://adatao.com/"
+            },
+            {
+                name: "Sticker Mule",
+                image: "img/stickermule.png",
+                url: "https://stickermule.com/"
+            },
+            {
+                name: "Brandmakers",
+                image: "img/brandmakers.jpg",
+                url: "http://brandmakers.com/"
+            },
+            {
+                name: "Lob",
+                image: "img/lob.png",
+                url: "https://lob.com/"
+            },
+            {
+                name: "Samsung",
+                image: "img/samsung.png",
+                url: "http://samsung.com/"
+            }
+        ]
+    },
+    rendered: function() {
+    }
+});
