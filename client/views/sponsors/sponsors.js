58181eb48dfd99871fb81102121f3a850c139f9e
Ryan Huang <npn.git@gmail.com>
2015-11-17 21:51:46 -0800

    Add Hacker Fund, Moxtra, and KPCB
    Also fixed some indentation and separated the sponsors into different
    categories.

--- client/views/sponsors/sponsors.js
+++ client/views/sponsors/sponsors.js
@@ -1,10 +1,34 @@
 Controller('sponsors', {
     helpers: {
-        sponsors: [
+        gold: [
+            {
+                name: "Moxtra",
+                image: "img/moxtra.png",
+                url: "http://www.moxtra.com/"
+            }
+        ],
+        bronze: [
             {
                 name: "Adatao",
                 image: "img/adatao.png",
                 url: "http://adatao.com/"
+            },
+            {
+                name: "Kleiner Perkins Caufield & Byers",
+                image: "img/kpcb.png",
+                url: "http://www.kpcb.com/"
+            }
+        ],
+        sponsor: [
+            {
+                name: "Lob",
+                image: "img/lob.png",
+                url: "https://lob.com/"
+            },
+            {
+                name: "Samsung",
+                image: "img/samsung.png",
+                url: "http://samsung.com/"
             },
             {
                 name: "Sticker Mule",
@@ -15,16 +39,6 @@ Controller('sponsors', {
                 name: "Brandmakers",
                 image: "img/brandmakers.jpg",
                 url: "http://brandmakers.com/"
-            },
-            {
-                name: "Lob",
-                image: "img/lob.png",
-                url: "https://lob.com/"
-            },
-            {
-                name: "Samsung",
-                image: "img/samsung.png",
-                url: "http://samsung.com/"
             }
         ]
     },
