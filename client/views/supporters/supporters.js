c1db3c141c6dece9ea7473d6207c40131be725e2
Ryan Huang <npn.git@gmail.com>
2016-01-11 17:37:30 -0800

    Fix sponsor classification and resize logos

--- client/views/supporters/supporters.js
+++ client/views/supporters/supporters.js
@@ -19,22 +19,7 @@ Controller('supporters', {
                 url: "http://www.kpcb.com/"
             }
         ],
-        communityPartner: [
-            {
-                name: "Hacker Fund",
-                image: "img/hackerfund.png",
-                url: "http://hacker.fund"
-            },
-            {
-                name: "Major League Hacking",
-                image: "img/mlh.png",
-                url: "https://mlh.io/"
-            },
-            {
-                name: "Make School",
-                image: "img/makeschool.png",
-                url: "https://www.makeschool.com/"
-            },
+        inKind: [
             {
                 name: "DigitalOcean",
                 image: "img/digitalocean.png",
@@ -44,11 +29,6 @@ Controller('supporters', {
                 name: "Lob",
                 image: "img/lob.png",
                 url: "https://lob.com/"
-            },
-            {
-                name: "Samsung",
-                image: "img/samsung.png",
-                url: "http://samsung.com/"
             },
             {
                 name: "GitHub",
@@ -69,6 +49,28 @@ Controller('supporters', {
                 name: "Brandmakers",
                 image: "img/brandmakers.jpg",
                 url: "http://brandmakers.com/"
+            }
+        ],
+        communityPartner: [
+            {
+                name: "Hacker Fund",
+                image: "img/hackerfund.png",
+                url: "http://hacker.fund"
+            },
+            {
+                name: "Make School",
+                image: "img/makeschool.png",
+                url: "https://www.makeschool.com/"
+            },
+            {
+                name: "Samsung",
+                image: "img/samsung.png",
+                url: "http://samsung.com/"
+            },
+            {
+                name: "Major League Hacking",
+                image: "img/mlh.png",
+                url: "https://mlh.io/"
             }
         ]
     },
