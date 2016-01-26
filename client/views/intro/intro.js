cd8eaab7a8d5b5467dd4c47bfb0fdf558a0fe6a2
Ryan Huang <npn.git@gmail.com>
2016-01-25 22:47:33 -0800

    Add address

--- client/views/intro/intro.js
+++ client/views/intro/intro.js
@@ -1,9 +1,11 @@
 Controller('intro', {
 
     helpers: {
-    	title: "Los Altos Hacks",
-    	location: "Microsoft Silicon Valley Campus",
-	    date: "January 30th-31st, 2016"
+        title: "Los Altos Hacks",
+        location: "Microsoft SVC Building 1",
+        address: "1065 La Avenida St, Mountain View",
+        mapLink: "https://www.google.com/maps/place/Microsoft+SVC+Building+1/@37.4115725,-122.0735347,17z/data=!3m1!4b1!4m2!3m1!1s0x808fb75a8f4aef1d:0xc1bc0e6d308e6c78", 
+        date: "January 30th-31st, 2016"
     },
 
     rendered: function() {
