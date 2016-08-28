fd9ca506529097ba95e09cbea3198e1f37f80864
Justin Yu <justin.v.yu@gmail.com>
2016-08-27 18:13:57 -0700

    Initial commit for ribbon site

--- js/afterparty.js
+++
@@ -1,3 +0,0 @@
-$("#show-arrow").click(function() {
-    $("html, body").animate({scrollTop: $("#ap-stats").offset().top}, 800);
-});
---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $("#navbar").sticky({topSpacing:0});
+});
