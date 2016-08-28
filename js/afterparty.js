73aa8bad9bff54d32201413c810e96cbc8ce7144
Dan Appel <dan.appel00@gmail.com>
2016-08-28 03:25:11 -0700

    Add basic email signup form

--- js/afterparty.js
+++ js/afterparty.js
@@ -1,3 +1,19 @@
 $("#show-arrow").click(function() {
     $("html, body").animate({scrollTop: $("#ap-stats").offset().top}, 800);
 });
+
+$("#email").keypress(function (e) {
+    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+        $('#submit').click();
+    }
+});
+
+$("#submit").click(function (e) {
+    var email = $("#email").val();
+    $.ajax({
+        type: "POST",
+        url: "http://api.losaltoshacks.com/subscribe",
+        data: JSON.stringify({ email: email }),
+        contentType: "application/json"
+    })
+});
