7193ee4788053abad3a003367a3dae13c5529832
Justin Yu <justin.v.yu@gmail.com>
2016-10-01 17:22:59 -0700

    Add sponsor logos + add student template

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $("#navbar").sticky({
-        topSpacing: 0,
-        responsiveBreakpoint: 558
-    });
+    // $("#navbar").sticky({
+    //     topSpacing: 0,
+    //     responsiveBreakpoint: 558
+    // });
 
     $("#navbar a").click(function () {
         $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 800);
