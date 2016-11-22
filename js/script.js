2357c22b78df4fb2d2c2b7351a8e02c1232acc6b
Ryan Huang <npn.git@gmail.com>
2016-11-21 16:11:34 -0800

    Reformat HTML/Mustache, remove old JS and comments
    The Mustache includes ( {{> partial}} ) have been unindented once to
    prevent prevent double-indenting in the output.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-    // $("#navbar").sticky({
-    //     topSpacing: 0,
-    //     responsiveBreakpoint: 558
-    // });
-
     $("#navbar a").click(function () {
         $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 800);
     });
