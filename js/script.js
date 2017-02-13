026f79bf9d4637391f01b76692fee1366398e733
Ryan Huang <npn.git@gmail.com>
2017-02-12 23:50:13 -0800

    Add winner carousel

--- js/script.js
+++ js/script.js
@@ -16,3 +16,23 @@ function addClickHandlers() {
 };
 
 ready(addClickHandlers);
+
+function setupFlickity() {
+    // We don't set this in CSS because if JS is disabled, we want the images
+    // to display naturally.
+    document.querySelector(".winner-carousel").style.height = "400px";
+
+    var projectName = document.querySelector(".winner-project-name"),
+        flickity = new Flickity(".winner-carousel", {
+        cellSelector: "img",
+        imagesLoaded: true
+    });
+
+    flickity.on("select", function () {
+        var image = flickity.selectedElement;
+        projectName.textContent = image.alt;
+        projectName.href = image.dataset.link;
+    });
+}
+
+ready(setupFlickity);
